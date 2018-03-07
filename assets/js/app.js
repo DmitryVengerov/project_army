97178accd34b8cb9b0c73663d2c3b2c71e12db48
Vengerov Dmitrij <hardbeat34@gmail.com>
2018-03-07 22:20:32 +0300

    profile page

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var Circle = function (sel) {
+  var circles = document.querySelectorAll(sel);
+  [].forEach.call(circles, function (el) {
+    var valEl = parseFloat(el.innerHTML)
+    valEl = valEl * 408 / 100
+    el.innerHTML = '<svg width="70" height="70"><circle transform="rotate(-90)" r="30" cx="-35" cy="35" /><circle transform="rotate(-90)" style="stroke-dasharray:' + valEl + 'px 408px;" r="30" cx="-35" cy="35" /></svg>'
+  })
+}
+Circle('.circle')
